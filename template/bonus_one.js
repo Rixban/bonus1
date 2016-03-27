5da1b90e11221549dd37abb63a8f00bd1358eda8
Rixban <ConanEtigawa24@aol.com>
2016-03-27 17:07:10 -0400

    TypeAhead
    Finished?

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -12,6 +12,68 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
+	var myArray; 
+	var userInput;
+	var entries = 0;
+	$(".flexsearch-suggestion").hide();
+	$.ajax({
+		type: 'GET',
+		url: 'http://www.mattbowytz.com/simple_api.json',
+		data: {query : userInput, data: "all"},
+		success: function (data) {
+			myArray = data.data.interests.concat(data.data.programming);
+			myArray.sort();
+             }
+	})
 	// Magic!
+
+	$( ".flexsearch-input" ).keyup(function() {
+		entries = 0;
+		$(".flexsearch-suggestion").hide();
+		userInput = document.querySelector('.flexsearch-input').value;
+		search(userInput);
+	});
+	
+	function search(data){
+		$("#suggestion").empty();
+		for(i = 0; i < myArray.length; i++){
+			if(myArray[i].toLowerCase().indexOf(data.toLowerCase()) != -1 && data !=""){
+				var match = true;
+				for(j = 0; j < data.length; j++){
+					if(myArray[i][j].toLowerCase() != data[j].toLowerCase()){
+						match = false;
+					}
+				}
+				if(match){
+					 var display=document.getElementById("suggestion");
+					$("#suggestion").append("<ul>"+myArray[i]+"</ul>");
+					entries++;
+							if(entries == 0){
+			$(".flexsearch-suggestion").hide();
+		}
+		else{
+			$(".flexsearch-suggestion").show();
+		}
+				}
+			}
+		}
+	}
+	
+	$( "#suggestion" ).on( "click", "ul", function() {
+		userInput = $(this).text();
+		document.querySelector('.flexsearch-input').value = userInput;
+		search(userInput);
+		window.location.href = "https://www.google.com/#q="+userInput;
+	});
+	
+	//$( "#suggestion" ).on( "click", function() {
+	
+	//});
+	
+
+	$( ".flexsearch-form" ).on( "submit", function redirect(e){
+		e.preventDefault();
+		window.location.href = "https://www.google.com/#q="+userInput;
+	});
 	console.log('Keepin\'n it clean with an external script!');
 })();
